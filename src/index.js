50da6c861fe343d5bd10e7cf2036d2b9db281882
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-04-25 17:25:10 -0300

    Terminando a função Luhn em Js

--- src/index.js
+++ src/index.js
@@ -1,53 +1,45 @@
-let numCard = document.getElementById("numberCard");
+import validator from './validator.js';
+
+let numCard = document.getElementById("numberCard").addEventListener("click", xxx, false);
 
 let sendData = document.getElementById("send");
 
+
+
 function isValidCard() {
-
-  let cardNumber = (numCard.value);
-
+  let card = str === "";
+//validação para que não entre uma String vazia
+    if(card){
+          alert("Escreva o número do seu cartão por favor");
+    }
+// use .value porque retorna o valor do atributo em um campo de texto
+  let cardNumber = (card.value);
+// .split para ir de string para array e .reverse para inverter os números no array
   let numberCardReverse = (cardNumber.split("")).reverse();
 
   console.log(numberCardReverse);
-
-  for(i = 0; i < numberCardReverse.length; i++){
-    if(i % 2 !== 0) {
-      if(numberCardReverse[i]* 2)}
-}
-console.log(multiplication);
-
-//duvida (suma de los digitos que sean mayor o igual a 10)
-for (i = 0; i < numberCardReverse.length; i++){
-  if(numberCardReverse[i].length === 2){ 
-    numberCardReverse[i] = parseInt(numberCardReverse[i][0]) + parseInt(numberCardReverse[i][1]);
-  } else {
-    numberCardReverse[i] = parseInt(numberCardReverse[i]);
-  }
-  
-}
-
-// suma de pares e impares (quienes son los impares?)
-
-
-//Sumo todos los digitos y si es multiplo de 10 la tarjeta es valida
-let sumArrayCard = 0;
+// devemos selecionar os números pares da array e multiplicá-los por 2
 for(i = 0; i < numberCardReverse.length; i++){
-    sumArrayCard += numberCardReverse[i];
+  if(i % 2 !== 0) {(numberCardReverse[i]* 2)}
+// suma de los digitos que sean mayor o igual a 10 (ParseInt: extrair número da string)
+if(numberCardReverse[i] >=10){ 
+  numberCardReverse[i] = parseInt(numberCardReverse[i][0]) + parseInt(numberCardReverse[i][1]);
+} else {
+  numberCardReverse[i] = parseInt(numberCardReverse[i]);  
+  }
 }
-console.log(sumArrayCard);
-if(sumArrayCard %10 == 0 ) {
+//Sumo todos los digitos y si es multiplo de 10 la tarjeta es valida
+let sumCard = 0;
+for(i = 0; i < numberCardReverse.length; i++){
+    sumCard += numberCardReverse[i];
+}
+console.log(sumCard);
+if(sumCard %10 == 0 ) {
     alert("Su tarjeta es valida");
 } else {
     alert("Su tarjeta no es valida");
 }
-
 }
-
-
-
-}
-
-
 
 
 
@@ -56,23 +48,3 @@ if(sumArrayCard %10 == 0 ) {
 
 
 // executado si condicao e atendida
-
-
-
-  
-
-// adicione os dígitos do resultado se o número for maior ou igual a 10, (firstValue---> salve o primeiro dígito que resultou da multiplicação e secondValue---> Armazene o segundo dígito do número resultante da multiplicação)
-if (multiplication >= 10)
-{
-  let firstValue= parseInt(multiplication/10);
-  let secondValue= multiplication%10; 
-  let sumDigitos= firstValue+secondValue;
-
-// obter números impares na array
-for(let k=0; k < numberCardReverse.length; k++){
-  if(i % 2 == 0)}
-
-
-
-
-}
