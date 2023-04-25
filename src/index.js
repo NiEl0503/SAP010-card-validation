ab3cb814d2c56b7776be2315d45e44f1e0261c1c
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-04-25 20:45:03 -0300

    Trabalhei em index.js e validator.js. Lógica e interação do código dividido

--- src/index.js
+++ src/index.js
@@ -1,50 +1,24 @@
 import validator from './validator.js';
 
-let numCard = document.getElementById("numberCard").addEventListener("click", xxx, false);
+//mostrar o número do cartão de crédito
 
-let sendData = document.getElementById("send");
+document.getElementById("send").addEventListener("click", validarnum);
 
-
-
-function isValidCard() {
-  let card = str === "";
+function nuCard() {
+  let numberCard = document.getElementById("number-Card").value;
+  let send = document.getElementById ("send");
+  let resultValid = validator.isValid(numberCard); // que representa validator aqui?
+  let numberBlock = validator.maskify(numberCard);
 //validação para que não entre uma String vazia
-    if(card){
-          alert("Escreva o número do seu cartão por favor");
+  if (numberCard === ""){
+    alert("Escreva o número do seu cartão por favor");
     }
-// use .value porque retorna o valor do atributo em um campo de texto
-  let cardNumber = (card.value);
-// .split para ir de string para array e .reverse para inverter os números no array
-  let numberCardReverse = (cardNumber.split("")).reverse();
+          else if(resultValid === true){
+        send.innerHTML = numberBlock + "Cartão válido";
+      }
+          else{
+        send.innerHTML = numberBlock + "Cartão inválido";
+      }
+     }
 
-  console.log(numberCardReverse);
-// devemos selecionar os números pares da array e multiplicá-los por 2
-for(i = 0; i < numberCardReverse.length; i++){
-  if(i % 2 !== 0) {(numberCardReverse[i]* 2)}
-// suma de los digitos que sean mayor o igual a 10 (ParseInt: extrair número da string)
-if(numberCardReverse[i] >=10){ 
-  numberCardReverse[i] = parseInt(numberCardReverse[i][0]) + parseInt(numberCardReverse[i][1]);
-} else {
-  numberCardReverse[i] = parseInt(numberCardReverse[i]);  
-  }
-}
-//Sumo todos los digitos y si es multiplo de 10 la tarjeta es valida
-let sumCard = 0;
-for(i = 0; i < numberCardReverse.length; i++){
-    sumCard += numberCardReverse[i];
-}
-console.log(sumCard);
-if(sumCard %10 == 0 ) {
-    alert("Su tarjeta es valida");
-} else {
-    alert("Su tarjeta no es valida");
-}
-}
-
-
-
-// document.getElementById('send').addEventListener('click',validar,false);
-
-
-
-// executado si condicao e atendida
+console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,36 @@
 const validator = {
-  // ...
-};
+  
+  isValid()  {
+  // use .value porque retorna o valor do atributo em um campo de texto
+    let cardNumber = (card.value);
+  // .split para ir de string para array e .reverse para inverter os números no array
+    let numberCardReverse = (cardNumber.split("")).reverse();
+  
+    console.log(numberCardReverse);
+  // devemos selecionar os números pares da array e multiplicá-los por 2
+  for(i = 0; i < numberCardReverse.length; i++){
+    if(i % 2 !== 0) {(numberCardReverse[i]* 2)}
+  // suma de los digitos que sean mayor o igual a 10 (ParseInt: extrair número da string)
+  if(numberCardReverse[i] >=10){ 
+    numberCardReverse[i] = parseInt(numberCardReverse[i][0]) + parseInt(numberCardReverse[i][1]);
+  } else {
+    numberCardReverse[i] = parseInt(numberCardReverse[i]);  
+    }
+  }
+  //Sumo todos los digitos y si es multiplo de 10 la tarjeta es valida
+  let sumCard = 0;
+  for(i = 0; i < numberCardReverse.length; i++){
+      sumCard += numberCardReverse[i];
+  }
+  console.log(sumCard);
+  if(sumCard %10 == 0 ) {
+    return true
+} else {
+    return false
+  }
+
+
+
+;
 
 export default validator;
