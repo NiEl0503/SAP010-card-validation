5d449a62b2cd6ebb727e46bdaf41de41d4ee65d3
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-04-26 18:13:06 -0300

    corrigindo bugs no validator.js

--- src/index.js
+++ src/index.js
@@ -2,23 +2,23 @@ import validator from './validator.js';
 
 //mostrar o número do cartão de crédito
 
-document.getElementById("send").addEventListener("click", validarnum);
+document.getElementById("send").addEventListener("click", IsValid);
 
-function nuCard() {
-  let numberCard = document.getElementById("number-Card").value;
-  let send = document.getElementById ("send");
-  let resultValid = validator.isValid(numberCard); // que representa validator aqui?
-  let numberBlock = validator.maskify(numberCard);
-//validação para que não entre uma String vazia
+function IsValid() {
+// use .value porque retorna o valor do atributo em um campo de texto
+  const numberCard = document.getElementById("numText").value;
+  const send = document.getElementById ("send");
+  const resultValid = validator.isValid(numberCard); // que representa validator aqui?
+  const numberBlock = validator.maskify(numberCard);
+  //validação para que não entre uma String vazia
   if (numberCard === ""){
     alert("Escreva o número do seu cartão por favor");
-    }
-          else if(resultValid === true){
-        send.innerHTML = numberBlock + "Cartão válido";
-      }
-          else{
-        send.innerHTML = numberBlock + "Cartão inválido";
-      }
-     }
-
+  }
+  else if(resultValid === true){
+    send.innerHTML = numberBlock + "Cartão válido";
+  }
+  else{
+    send.innerHTML = numberBlock + "Cartão inválido";
+  }
+}
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,34 +1,33 @@
 const validator = {
   
   isValid()  {
-  // use .value porque retorna o valor do atributo em um campo de texto
-    let cardNumber = (card.value);
-  // .split para ir de string para array e .reverse para inverter os números no array
-    let numberCardReverse = (cardNumber.split("")).reverse();
-  
-    console.log(numberCardReverse);
-  // devemos selecionar os números pares da array e multiplicá-los por 2
-  for(i = 0; i < numberCardReverse.length; i++){
-    if(i % 2 !== 0) {(numberCardReverse[i]* 2)}
-  // suma de los digitos que sean mayor o igual a 10 (ParseInt: extrair número da string)
-  if(numberCardReverse[i] >=10){ 
-    numberCardReverse[i] = parseInt(numberCardReverse[i][0]) + parseInt(numberCardReverse[i][1]);
-  } else {
-    numberCardReverse[i] = parseInt(numberCardReverse[i]);  
+    const numberCard = document.getElementById("numText").value;
+    // .split para ir de string para array e .reverse para inverter os números no array
+    const numberCardReverse = (numberCard.split("")).reverse();
+    // devemos selecionar os números pares da array e multiplicá-los por 2
+    // i valor inicial da variável de controle da repetição
+    let i = numberCard;
+    for(i = 0; i < numberCardReverse.length; i++){
+      if(i % 2 === 0) {(numberCardReverse[i]* 2)}
+      // suma de los digitos que sean mayor o igual a 10. ParseInt: extrair número da string
+      if(numberCardReverse[i] >=10){ 
+        numberCardReverse[i] = parseInt(numberCardReverse[i][0]) + parseInt(numberCardReverse[i][1]);
+      } else {
+        numberCardReverse[i] = parseInt(numberCardReverse[i]);  
+      }
+    }
+    //Sumo todos los digitos y si es multiplo de 10 la tarjeta es valida
+    let sumCard = 0;
+    for(i = 0; i < numberCardReverse.length; i++){
+      sumCard += numberCardReverse[i];
+    }
+    if(sumCard %10 === 0 ) {
+      return true
+    } else {
+      return false
     }
   }
-  //Sumo todos los digitos y si es multiplo de 10 la tarjeta es valida
-  let sumCard = 0;
-  for(i = 0; i < numberCardReverse.length; i++){
-      sumCard += numberCardReverse[i];
-  }
-  console.log(sumCard);
-  if(sumCard %10 == 0 ) {
-    return true
-} else {
-    return false
-  }
-
+}
 
 
 ;
