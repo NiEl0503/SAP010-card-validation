5c7c9e1d7e690446095fa8e951b04dd360441ee5
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-05-02 16:30:47 -0300

    Updates

--- validator.js
+++ validator.js
@@ -1,25 +1,26 @@
 const validator = {
-  
-  isValid: function(numberC) {  
+
+  isValid: function (numberC) {
     const numberCardReverse = (numberC.split("")).reverse();
     // devemos selecionar os números pares da array e multiplicá-los por 2
     // i valor inicial da variável de controle da repetição
-    for(let i = 0; i < numberCardReverse.length; i++){
-      if(i % 2 === 0) {numberCardReverse[i] = (numberCardReverse[i]* 2)
-      // suma de los digitos que sean mayor o igual a 10. ParseInt: extrair número da string
-        if(numberCardReverse[i] >=10){ 
+    for (let i = 0; i < numberCardReverse.length; i++) {
+      if (i % 2 === 0) {
+        numberCardReverse[i] = (numberCardReverse[i] * 2)
+        // suma de los digitos que sean mayor o igual a 10. ParseInt: extrair número da string
+        if (numberCardReverse[i] >= 10) {
           numberCardReverse[i] = parseInt(numberCardReverse[i][0]) + parseInt(numberCardReverse[i][1]);
-        } 
-      } 
-      else {
-        numberCardReverse[i] = parseInt(numberCardReverse[i]);  
-      }
-      let sumCard = 0;
-      for(let i = 0; i < numberCardReverse.length; i++){
-        if(i % 2 !== 0) {sumCard = parseInt(numberCardReverse[i])
         }
       }
-      if(sumCard %10 === 0 ) {
+      else {
+        numberCardReverse[i] = parseInt(numberCardReverse[i]);
+      }      let sumCard = 0;
+      for (let i = 0; i < numberCardReverse.length; i++) {
+        if (i % 2 !== 0) {
+          sumCard = parseInt(numberCardReverse[i])
+        }
+      }
+      if (sumCard % 10 === 0) {
         return true
       } else {
         return false
@@ -28,7 +29,7 @@ const validator = {
   },
 
   maskify: function (numberC) {
-  // número é transformado em string para iterar no for
+    // número é transformado em string para iterar no for
     const mask = numberC.toString();
     let maskNum = "";
     for (let i = 0; i < mask.length; i++) {
