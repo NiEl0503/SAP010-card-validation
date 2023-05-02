ba2e0582681aa7e60b2674c9bd246fdc097be32a
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2023-05-02 19:47:52 -0300

    Updates

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ import validator from './validator.js';
 
 //mostrar o número do cartão de crédito
 const numberCard = document.getElementById("send");
-numberCard.addEventListener("click", isValid)  
+numberCard.addEventListener("click", isValid)
 function isValid() {
   // use .value porque retorna o valor do atributo em um campo de texto
   const numberC = document.getElementById("numText").value;
-  const resultValid = validator.isValid(numberC); 
+  const resultValid = validator.isValid(numberC);
   const numberBlock = validator.maskify(numberC);
   //validação para que não entre uma String vazia
-  if (numberC === ""){
+  if (numberC === "") {
     alert("Escreva o número do seu cartão por favor");
   }
-  else if (resultValid === true){
+  else if (resultValid === true) {
     numberCard.innerHTML = numberBlock + "Cartão válido";
   }
-  else{
+  else {
     numberCard.innerHTML = numberBlock + "Cartão inválido";
   }
 }
